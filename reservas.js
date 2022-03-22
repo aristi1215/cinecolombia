500155f5dad87606ebf5f90945061d14a7122f0a
unknown <juan pablo aristizabal>
2022-03-22 17:17:51 -0500

    reservas.js

--- reservas.js
+++ reservas.js
@@ -1,4 +1,15 @@
 let contenedor=document.getElementById("contenedor")
+
 contenedor.addEventListener("click",function(evento){
-if(evento.target.classList.contains("w-100"))
+
+    if(evento.target.classList.contains("w-100")){
+
+        evento.target.src="imagenes/cinema-silla3.png"
+
+    }
+
 })
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function)(evento){
+    alert("gran total")
+}
